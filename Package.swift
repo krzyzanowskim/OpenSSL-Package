2bc69692f9f9967f298ca768f26ed85b0cb08788
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2025-03-26 17:08:34 +0100

    3.3.3000

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenSSL",
-            url: "https://github.com/krzyzanowskim/OpenSSL/releases/download/3.3.2000/OpenSSL.xcframework.zip",
-            checksum: "41d034ea1c075bfa74048e851358a550996c286de8230d1df39f137b06235c87"
+            url: "https://github.com/krzyzanowskim/OpenSSL/releases/download/3.3.3000/OpenSSL.xcframework.zip",
+            checksum: "9b8e572e048978952fc6c9864a256a369637f0bbf10f0c58a703b9c001215b2f"
         )
     ]
 )
